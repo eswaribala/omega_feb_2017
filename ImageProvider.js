5437ec8f70caa22f4857e4f1199d7eeb16d6ca42
eswaribala <Parameswaribala@gmail.com>
2017-02-10 14:08:48 +0530

    Add files via upload

---
+++ ImageProvider.js
@@ -0,0 +1,24 @@
+﻿var imageModule = angular.module('ImageModule', []);
+imageModule.provider('test', function () {
+   
+    var Logo = '';
+
+    
+    this.setLogo = function (newLogo) {
+        Logo = newLogo;
+    };
+
+    this.$get = function () {
+        return {
+          
+            logo: Logo
+        };
+    };
+});
+
+//configuring provider----append provider to provider name
+imageModule.config(function (testProvider) {
+  
+    testProvider.setLogo("images/omega.jpg");
+
+});
